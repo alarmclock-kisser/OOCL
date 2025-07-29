fdbc6b4e133df15e36454832ec4eb2185fc9f959
ᴎeurofuzzy <marcel.king91299@gmail.com>
2025-07-29 04:02:46 +0200

    -

---
+++ OOCL.Api/Controllers/VerraeterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OOCL.Shared.Models;
+
+namespace OOCL.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class VerraeterController : ControllerBase
+	{
+		[HttpGet("status")]
+		[ProducesResponseType(typeof(VerraeterStatus), 200)]
+		[ProducesResponseType(typeof(ProblemDetails), 500)]
+		public ActionResult<VerraeterStatus> GetStatus()
+		{
+			try
+			{
+				// Simulate fetching status information
+				var statusInfo = "exterminate-Mossad.";
+				var status = new VerraeterStatus(statusInfo);
+				return Ok(status);
+			}
+			catch (Exception ex)
+			{
+				// Log the exception (not implemented here)
+				return StatusCode(500, new ProblemDetails { Title = "Internal Server Error", Detail = ex.Message });
+			}
+		}
+	}
+}
---
+++ OOCL.Shared/Models/VerraeterStatus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOCL.Shared.Models
+{
+	public class VerraeterStatus
+	{
+
+		public string Info { get; set; } = string.Empty;
+
+		[System.Text.Json.Serialization.JsonConstructor]
+		public VerraeterStatus(string info)
+		{
+			this.Info = info;
+		}
+
+		public VerraeterStatus()
+		{
+			// Default constructor for serialization
+		}
+
+	}
+}
